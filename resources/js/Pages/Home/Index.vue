bd4ab53f09a01849a423edf7e5cfbaceebd79a27
pmoreira <pmoreira@firenzze.com>
2023-10-13 20:46:23 -0300

    feat(home): add home basic layout

---
+++ resources/js/Pages/Home/Index.vue
@@ -0,0 +1,34 @@
+<template>
+
+    <div class="p-home font-sans text-[#140420]/[0.87] text-base">
+        <header class=" w-full h-24 bg-white grid items-center px-28">
+            <img class="h-[50px] w-auto" src="logo.png" alt="logo diversatlas">
+        </header>
+
+        <main class="w-full p-28">
+            <section class="grid grid-cols-2">
+                <header class="grid gap-6 text-left">
+                    <h1 class=" text-5xl font-black text-[#05EBFD]/[0.87]">Título CTA vai aqui neste lugar</h1>
+
+                    <p class="text-xl text-[#ffff]/[0.87]">Lorem ipsum dolor sit amet consectetur. Laoreet morbi amet suspendisse egestas varius sit tellus turpis. Adipiscing morbi quis eu eu ridiculus ornare quam.</p>
+
+                    <button class=" flex py-2 px-3 bg-[#FFCB32] text-[#140420]/[0.87] w-fit rounded-lg text-base font-bold" >Faça parte</button>
+                </header>
+
+                <div class="w-full max-w-full">
+
+                </div>
+            </section>
+        </main>
+    </div>
+
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.p-home {
+    background: linear-gradient(180deg, rgba(145, 0, 255, 0.20) 0%, rgba(255, 105, 122, 0.20) 100%), #6412A2;
+}
+</style>
