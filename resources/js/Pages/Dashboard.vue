b21763cc33f0a2573cd528b5b314c3d28d3640bf
Diogo Antunes <diogo.antunes@atlastechnol.com>
2023-10-13 19:24:05 -0300

    feat: add p1 page structure

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -14,7 +14,23 @@ import Welcome from '@/Components/Welcome.vue';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
+                    <div class="flex p-5 h-64  dark:text-white justify-center items-center">
+                        <span>Curso em progresso</span>
+                    </div>
+                </div>
+
+                <div class="p-5 mt-5 bg-white dark:bg-gray-800 shadow-xl sm:rounded-lg  overflow-x-auto">
+                    <div class="flex w-fit dark:text-white">
+                        <div v-for="i in 11" :key="i" class="p-2 mb-5 mr-5 last:mr-0 w-64 h-32 bg-gray-200 dark:bg-gray-900 rounded-lg">
+                            <span>Curso em progresso</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mt-5 bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                    <div class="flex p-5 h-64 dark:text-white justify-center items-center">
+                        <span>Materiais</span>
+                    </div>
                 </div>
             </div>
         </div>
