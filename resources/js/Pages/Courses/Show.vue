02218eb115c59138e5a25e82565039b05f53cc46
Diogo Antunes <diogo.antunes@atlastechnol.com>
2023-10-13 22:54:46 -0300

    feat: add subscribe button

--- resources/js/Pages/Courses/Show.vue
+++ resources/js/Pages/Courses/Show.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
+import { useForm } from '@inertiajs/vue3'
 </script>
 
 <template>
@@ -13,18 +14,21 @@ import AppLayout from "@/Layouts/AppLayout.vue";
     </template>
 
     <div class="pb-10">
-        <div class="pt-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="pt-12 max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="p-5 bg-white dark:bg-gray-800 dark:text-white shadow-xl rounded-lg">
                 <div class="mx-auto w-full max-w-5xl rounded-lg">
                     <img src="/storage/images/courses/banners/ff5c688d-5414-4eb7-83a0-ea32d90a8e92.png" :alt="course.title" class="w-full h-64" />
                 </div>
+                <div class="mt-5 mx-auto w-fit">
+                    <button @mouseover="mouseOnUnsubscribe = true" @mouseleave="mouseOnUnsubscribe = false" @click="subscribe" class="border rounded-md py-3 px-5 hover:bg-gray-400 hover:text-white dark:hover:bg-white dark:hover:text-black w-40">
+                        <span v-if="isSubscribed">{{ mouseOnUnsubscribe ? 'Desinscrever-se' : 'Já inscrito!' }}</span>
+                        <span v-else>Inscrever-se</span>
+                    </button>
+                </div>
             </div>
         </div>
-        </div>
 
-        <div class="mt-10">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="mt-10 max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 dark:text-white shadow-xl sm:rounded-lg">
                 <div class="p-5">
                     <span class="text-bold text-lg">Informações</span>
@@ -48,7 +52,6 @@ import AppLayout from "@/Layouts/AppLayout.vue";
                 </div>
             </div>
         </div>
-        </div>
 
         <div class="mt-10">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -70,30 +73,21 @@ import AppLayout from "@/Layouts/AppLayout.vue";
 <script>
 export default {
     props: {
-        course: Object
+        course: Object,
+        isSubscribed: Boolean
     },
     data() {
         return {
-        title: "PHP do básico ao avançado",
-        video: "https://www.youtube.com/embed/zZ6vybT1HQs",
-        classes: [
-            {
-            id: 1,
-            name: "Instalando o ambiente",
-            video: "https://www.youtube.com/embed/zZ6vybT1HQs",
-            },
-            {
-            id: 2,
-            name: "Apertando os cintos",
-            video: "https://www.youtube.com/embed/zZ6vybT1HQs",
-            },
-            {
-            id: 3,
-            name: "Decolando",
-            video: "https://www.youtube.com/embed/zZ6vybT1HQs",
-            },
-        ],
+            mouseOnUnsubscribe: false,
+            formSubscribe: useForm({
+                course_id: this.course.id
+            })
         };
     },
+    methods: {
+        subscribe: function () {
+            this.formSubscribe.post(route('courses.subscribe'))
+        }
+    }
 };
 </script>
