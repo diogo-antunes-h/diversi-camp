356910e9a9e5f987843eaf7e769004bc89be1d7f
Diogo Antunes <diogo.antunes@atlastechnol.com>
2023-10-14 11:13:10 -0300

    feat: add user course seeder

---
+++ database/seeders/UserCourseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Course;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserCourseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $courses = Course::all();
+        $users = User::all();
+
+        foreach ($courses as $course) {
+            foreach ($users as $user) {
+                $isSubscribed = (bool)random_int(0, 1);
+
+                if (! $isSubscribed) continue;
+
+                $user->courses()->save($course);
+            }
+        }
+    }
+}
