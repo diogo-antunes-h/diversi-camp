760404d846b115f07b6e158fb6fe2b6da2a0c34b
Diogo Antunes <diogo.antunes@atlastechnol.com>
2023-10-14 00:18:32 -0300

    feat: add new course relation

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Course extends Model
 {
@@ -29,5 +29,10 @@ class Course extends Model
     public function lessons()
     {
         return $this->hasMany(Lesson::class, 'course_id', 'id')->orderBY('order');
+    }
+
+    public function subscriptions(): HasMany
+    {
+        return $this->hasMany(UserCourse::class);
     }
 }
