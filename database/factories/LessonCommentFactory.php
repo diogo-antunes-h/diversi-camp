fccf5bf142770897014b3583e5a927b1de326307
Diogo Antunes <diogo.antunes@atlastechnol.com>
2023-10-14 11:43:47 -0300

    feat: create lesson comment factory

---
+++ database/factories/LessonCommentFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Course>
+ */
+class LessonCommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'comment' => $this->faker->text(random_int(10, 200)),
+        ];
+    }
+}
