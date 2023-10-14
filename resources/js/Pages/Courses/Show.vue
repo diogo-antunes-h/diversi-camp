0aa569c028fdd07e6c44527296d0900d72798556
Diogo Antunes <diogo.antunes@atlastechnol.com>
2023-10-13 21:33:20 -0300

    chore: done layout improvements

--- resources/js/Pages/Courses/Show.vue
+++ resources/js/Pages/Courses/Show.vue
@@ -12,46 +12,57 @@ import AppLayout from "@/Layouts/AppLayout.vue";
       </h2>
     </template>
 
-    <div class="pt-12">
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="p-5 bg-white dark:bg-gray-800 dark:text-white shadow-xl rounded-lg">
-            <div class="mx-auto w-full max-w-5xl rounded-lg">
-                <img src="/storage/images/courses/banners/ff5c688d-5414-4eb7-83a0-ea32d90a8e92.png" :alt="course.title" class="w-full h-64" />
+    <div class="pb-10">
+        <div class="pt-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="p-5 bg-white dark:bg-gray-800 dark:text-white shadow-xl rounded-lg">
+                <div class="mx-auto w-full max-w-5xl rounded-lg">
+                    <img src="/storage/images/courses/banners/ff5c688d-5414-4eb7-83a0-ea32d90a8e92.png" :alt="course.title" class="w-full h-64" />
+                </div>
             </div>
         </div>
-      </div>
-    </div>
+        </div>
 
-    <div class="mt-10">
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white dark:bg-gray-800 dark:text-white shadow-xl sm:rounded-lg" style="min-height: 500px;">
-            <div class="p-5">
-                <span class="text-bold text-lg">Informações</span>
+        <div class="mt-10">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 dark:text-white shadow-xl sm:rounded-lg">
+                <div class="p-5">
+                    <span class="text-bold text-lg">Informações</span>
 
-                <div class="mt-5 p-5 flex flex-wrap">
+                    <div class="mt-10 grid grid-cols-4">
                         <div class="flex flex-col">
                             <span class="font-bold">Título</span>
                             <span>{{ course.title }}</span>
                         </div>
 
-                        <div class="flex flex-col ml-auto">
+                        <div class="flex flex-col">
                             <span class="font-bold">Slug</span>
                             <span>{{ course.slug }}</span>
                         </div>
 
-                        <div class="flex flex-col ml-auto">
-                            <span class="font-bold">Professor</span>
-                            <span>{{ course.creator.name }}</span>
-                        </div>
-
-                        <div class="flex flex-col ml-auto">
+                        <div class="flex flex-col">
                             <span class="font-bold">Criado em</span>
                             <span>{{ course.created_at }}</span>
                         </div>
+                    </div>
                 </div>
             </div>
         </div>
-      </div>
+        </div>
+
+        <div class="mt-10">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 dark:text-white shadow-xl sm:rounded-lg">
+                <div class="p-5">
+                    <span class="text-bold text-lg">Descrição</span>
+
+                    <div class="mt-10 flex">
+                        <span>{{ course.description }}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
     </div>
   </AppLayout>
 </template>
