91af8aa0c6d2b2925b7ea02e8eada8125ca48f1a
Diogo Antunes <diogo.antunes@atlastechnol.com>
2023-10-14 11:02:43 -0300

    feat: remove lesson seeding from course seeder

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -22,38 +22,10 @@ class CourseSeeder extends Seeder
     {
         $title = 'PHP do básico ao avançado';
 
-        $course = Course::create([
+        Course::create([
             'title' => $title,
             'slug' => str($title)->slug(),
             'description' => fake()->text(600),
         ]);
-
-        $lessons = [
-            [
-                'course_id' => $course->id,
-                'title' => 'Montando o ambiente',
-                'description' => 'Loren ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, quis aliquam nisl',
-                'path' => 'https://www.youtube.com/embed/o7NRCy-dCMk',
-                'order' => 2,
-            ],
-            [
-                'course_id' => $course->id,
-                'title' => 'Primeiros passos com PHP',
-                'description' => 'Loren ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, quis aliquam nisl',
-                'path' => 'https://www.youtube.com/embed/Q_FgNKPSi74',
-                'order' => 3,
-            ],
-            [
-                'course_id' => $course->id,
-                'title' => 'Conhecendo o PHP',
-                'description' => 'Loren ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, quis aliquam nisl',
-                'path' => 'https://www.youtube.com/embed/JlPqHQuYD4Y',
-                'order' => 4,
-            ],
-        ];
-
-        foreach ($lessons as $lesson) {
-            Lesson::create($lesson);
-        }
     }
 }
