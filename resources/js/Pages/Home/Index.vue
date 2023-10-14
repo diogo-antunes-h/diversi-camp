6afc24de498b202b8a982ce9448493d35343967d
pmoreira <pmoreira@firenzze.com>
2023-10-13 21:10:13 -0300

    feat(home): add cards section

--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -8,7 +8,7 @@
         <main class="w-full p-28">
             <section class="grid grid-cols-2">
                 <header class="grid gap-6 text-left">
-                    <h1 class=" text-5xl font-black text-[#05EBFD]/[0.87]">Título CTA vai aqui neste lugar</h1>
+                    <h1 class="text-5xl font-black text-[#05EBFD]/[0.87]">Título CTA vai aqui neste lugar</h1>
 
                     <p class="text-xl text-[#ffff]/[0.87]">Lorem ipsum dolor sit amet consectetur. Laoreet morbi amet suspendisse egestas varius sit tellus turpis. Adipiscing morbi quis eu eu ridiculus ornare quam.</p>
 
@@ -16,6 +16,42 @@
                 </header>
 
                 <div class="w-full max-w-full">
+
+                </div>
+            </section>
+
+            <section class="o-section pt-28">
+                <div class="o-wrapper grid grid-cols-3 gap-5">
+
+                    <article class="o-card grid gap-6 text-[#ffff]/[0.87] p-6 rounded-lg border border-[#ffffff]/[0.2] bg-[#fff]/[0.1]">
+                        <header class="m-card__header">
+                            <img src="" alt="" class="a-card-icon w-[62px] h-[62px]">
+                        </header>
+                        <div class="m-card__body grid gap-4">
+                            <h3 class="a-card-title text-[#05EBFD]/[0.87] text-2xl font-black">Nossos cursos</h3>
+                            <p class="a-card-text">Lorem ipsum dolor sit amet consectetur. Laoreet morbi amet suspendisse egestas varius sit tellus turpis. Adipiscing morbi quis eu eu ridiculus ornare quam.</p>
+                        </div>
+                    </article>
+
+                    <article class="o-card grid gap-6 text-[#ffff]/[0.87] p-6 rounded-lg border border-[#ffffff]/[0.2] bg-[#fff]/[0.1]">
+                        <header class="m-card__header">
+                            <img src="" alt="" class="a-card-icon w-[62px] h-[62px]">
+                        </header>
+                        <div class="m-card__body grid gap-4">
+                            <h3 class="a-card-title text-[#05EBFD]/[0.87] text-2xl font-black">Nossos cursos</h3>
+                            <p class="a-card-text">Lorem ipsum dolor sit amet consectetur. Laoreet morbi amet suspendisse egestas varius sit tellus turpis. Adipiscing morbi quis eu eu ridiculus ornare quam.</p>
+                        </div>
+                    </article>
+
+                    <article class="o-card grid gap-6 text-[#ffff]/[0.87] p-6 rounded-lg border border-[#ffffff]/[0.2] bg-[#fff]/[0.1]">
+                        <header class="m-card__header">
+                            <img src="" alt="" class="a-card-icon w-[62px] h-[62px]">
+                        </header>
+                        <div class="m-card__body grid gap-4">
+                            <h3 class="a-card-title text-[#05EBFD]/[0.87] text-2xl font-black">Nossos cursos</h3>
+                            <p class="a-card-text">Lorem ipsum dolor sit amet consectetur. Laoreet morbi amet suspendisse egestas varius sit tellus turpis. Adipiscing morbi quis eu eu ridiculus ornare quam.</p>
+                        </div>
+                    </article>
 
                 </div>
             </section>
@@ -30,5 +66,9 @@
 <style scoped>
 .p-home {
     background: linear-gradient(180deg, rgba(145, 0, 255, 0.20) 0%, rgba(255, 105, 122, 0.20) 100%), #6412A2;
+}
+
+.o-card {
+    box-shadow: 0px 30px 100px 0px rgba(0, 0, 0, 0.20);
 }
 </style>
