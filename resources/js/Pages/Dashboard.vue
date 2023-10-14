e624052a49ce74bbad2bae61708576855c073158
Guilherme Mendes <guilherme8787@gmail.com>
2023-10-13 23:30:03 -0300

    feat: dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,38 +1,281 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Components/Welcome.vue';
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Welcome from "@/Components/Welcome.vue";
 </script>
 
 <template>
-    <AppLayout title="Dashboard">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                Dashboard
-            </h2>
-        </template>
+  <AppLayout title="Dashboard">
+    <template #header>
+      <h2
+        class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight"
+      >
+        Dashboard
+      </h2>
+    </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                    <div class="flex p-5 h-64  dark:text-white justify-center items-center">
-                        <span>Curso em progresso</span>
-                    </div>
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div
+          class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg"
+        >
+          <div
+            class="flex p-5 h-auto dark:text-white justify-center items-center"
+          >
+            <img
+              class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+              src="https://d36ai2hkxl16us.cloudfront.net/thoughtindustries/image/upload/a_exif,c_fill,w_750,h_361/v1/course-uploads/4338ce4e-f809-4f5a-80f4-1d317c4a390d/lsczjuajit2k-38a0f59507j0-php-course-thumbnail.png"
+              alt=""
+            />
+            <div class="flex flex-col justify-between p-4 leading-normal">
+              <h5
+                class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+              >
+                PHP do básico ao avançado
+              </h5>
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                Aprenda essa poderosa ferramenta com os melhores instrutoes
+              </p>
+              <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                <div
+                  class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
+                  style="width: 45%"
+                >
+                  45%
                 </div>
-
-                <div class="p-5 mt-5 bg-white dark:bg-gray-800 shadow-xl sm:rounded-lg  overflow-x-auto">
-                    <div class="flex w-fit dark:text-white">
-                        <div v-for="i in 11" :key="i" class="p-2 mb-5 mr-5 last:mr-0 w-64 h-32 bg-gray-200 dark:bg-gray-900 rounded-lg">
-                            <span>Curso em progresso</span>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="mt-5 bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                    <div class="flex p-5 h-64 dark:text-white justify-center items-center">
-                        <span>Materiais</span>
-                    </div>
-                </div>
+              </div>
             </div>
+            <a
+              href="#"
+              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Continuar
+              <svg
+                class="w-3.5 h-3.5 ml-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+          </div>
         </div>
-    </AppLayout>
+
+        <div
+          class="grid grid-cols-3 gap-3 p-5 mt-5 bg-white dark:bg-gray-800 shadow-xl sm:rounded-lg"
+        >
+          <!-- php -->
+          <div class="flex w-full dark:text-white">
+            <div
+              class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+            >
+              <a href="#">
+                <img
+                  class="rounded-t-lg h-80 object-cover"
+                  src="https://d36ai2hkxl16us.cloudfront.net/thoughtindustries/image/upload/a_exif,c_fill,w_750,h_361/v1/course-uploads/4338ce4e-f809-4f5a-80f4-1d317c4a390d/lsczjuajit2k-38a0f59507j0-php-course-thumbnail.png"
+                  alt=""
+                />
+              </a>
+              <div class="p-5">
+                <a href="#">
+                  <h5
+                    class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+                  >
+                    PHP do básico ao avançado
+                  </h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  Aprenda essa poderosa ferramenta com os melhores instrutoes
+                </p>
+                <a
+                  href="#"
+                  class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Inscrito
+                  <svg
+                    class="w-3.5 h-3.5 ml-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <!-- diversidade -->
+          <div class="flex w-full dark:text-white">
+            <div
+              class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+            >
+              <a href="#">
+                <img
+                  class="rounded-t-lg h-80 object-cover"
+                  src="https://classic.exame.com/wp-content/uploads/2021/06/GettyImages-1288712636.jpg?quality=70&strip=info&w=1024"
+                  alt=""
+                />
+              </a>
+              <div class="p-5">
+                <a href="#">
+                  <h5
+                    class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+                  >
+                    Diversidade no ambiente corporativo
+                  </h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  Aprenda a promover a diversidade no ambiente corporativo
+                </p>
+                <a
+                  href="#"
+                  class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Inscrever-se
+                  <svg
+                    class="w-3.5 h-3.5 ml-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <!-- go lang -->
+          <div class="flex w-full dark:text-white">
+            <div
+              class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+            >
+              <a href="#">
+                <img
+                  class="rounded-t-lg h-80 object-cover"
+                  src="https://pbs.twimg.com/media/E3_rJZvWUAQLWYy?format=jpg&name=large"
+                  alt=""
+                />
+              </a>
+              <div class="p-5">
+                <a href="#">
+                  <h5
+                    class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+                  >
+                    Go Lang
+                  </h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  Aprenda essa super poderosa e moderna linguagem de programação
+                </p>
+                <a
+                  href="#"
+                  class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Inscrever-se
+                  <svg
+                    class="w-3.5 h-3.5 ml-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div
+          class="mt-5 bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg"
+        >
+          <div
+            class="grid grid-cols-6 gap-3 flex p-5 h-auto dark:text-white justify-center items-center"
+          >
+            <span>Materiais</span>
+
+            <div
+              v-for="book in books"
+              :key="book.id"
+              class="flex w-full dark:text-white"
+            >
+              <div
+                class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+              >
+                <a href="#">
+                  <img
+                    v-bind:src="book.volumeInfo.imageLinks.thumbnail"
+                    class="rounded-t-lg h-80 object-cover"
+                    alt=""
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </AppLayout>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      books: [],
+    };
+  },
+  mounted() {
+    const url =
+      "https://books.googleapis.com/books/v1/volumes?q=programming&maxResults=10&fields=items/volumeInfo(authors,imageLinks,pageCount,publisher,publishedDate,previewLink,title)";
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Erro na resposta da rede");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.items) {
+          this.books = data.items;
+          console.log(this.books);
+        } else {
+          console.log("Nenhum livro encontrado.");
+        }
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar os dados:", error);
+      });
+  },
+};
+</script>
